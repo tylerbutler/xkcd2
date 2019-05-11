7191c8a8d818649b1f6778b84726d7b38bb6ef1c
Tyler Butler <tyler@tylerbutler.com>
2019-05-11 15:20:57 -0700

    Remove Python files

--- index.py
+++
@@ -1,9 +0,0 @@
-# coding=utf-8
-import sys
-from path import path
-
-# Ensure the directory containing xkcd2.py is on the python path
-sys.path.insert(0, str(path(__file__).dirname()))
-
-# import the Flask app so mod_wsgi can run it
-from xkcd2 import app as application
